4cd00c2520568770eb008fed733a85f5fea67e7f
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-09 20:45:31 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.0"
     id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.2"
+    id("maven-publish")
 }
 
 group = "studio.hcmc"
@@ -10,14 +11,26 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation(project(":kotlin-protocol-extension"))
-
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.4.0")
+    maven { setUrl("https://jitpack.io") }
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "studio.hcmc"
+            artifactId = "spring-controller-extension"
+            version = "0.0.4"
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    implementation("com.github.hcmc-studio:kotlin-protocol-extension:0.0.4-build-02")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.4.0")
 }
