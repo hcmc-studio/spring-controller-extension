8bd73cf8bc09a15e48dba26e854a2d32f1940eba
Ji-Hwan KIM <nodealdev@gmail.com>
2023-08-09 21:04:06 +0900

    [Script push]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.spring") version "1.9.0"
-    id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.2"
     id("maven-publish")
 }
 
 group = "studio.hcmc"
-version = "1.0-SNAPSHOT"
+version = "0.0.4"
 
 repositories {
     mavenCentral()
